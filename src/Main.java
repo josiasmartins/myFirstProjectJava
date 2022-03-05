5e04f685110406c6343c30a7a4854c1b42302b62
Caitano <josiasmartinscaitano@gmail.com>
2022-03-04 22:41:55 -0300

    adicionado comentário

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,15 @@ public class Main {
     public static void main(String[] args) {
         // TODO: Auto-generetade method stub;
 
-        System.out.println("I love pizza");
+        System.out.print("I love pizza\n");
+        System.out.print("It's really good");
+
+        // This is comment
+
+        /*
+        *   This
+        *   is
+        *   comment
+        */
     }
 }
