770dd919dfd4243c59cf64a605eda41cedebf425
Caitano <josiasmartinscaitano@gmail.com>
2022-03-04 23:21:11 -0300

    adicioando expression

---
+++ src/Expression.java
@@ -0,0 +1,15 @@
+public class Expression {
+
+    public static void main(String[] args) {
+
+        // expression = operands & operators
+        // operands = Values, Variables, Numbers, quantity
+        // operators = + - * / %;
+
+        int friends = 10;
+
+        friends--;
+
+        System.out.println(friends);
+    }
+}
